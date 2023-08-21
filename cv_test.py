1cd0f655d0147c4dc2aea90c8866e86a16cbf693
lingfliu <lingfeng.liu@163.com>
2023-08-21 09:09:19 +0800

    新增光流可视化脚本

--- cv_test.py
+++ cv_test.py
@@ -12,9 +12,40 @@
 #     if cv2.waitKey(2) & 0xFF == ord('q'):
 #         break
 
+# import numpy as np
+#
+# a = np.ones(10,20,2)
+# b = np.empty((0,10,20,2))
+#
+# np.append(b, np.array([a]), axis=0)
+
+import cv2
 import numpy as np
 
-a = np.ones(10,20,2)
-b = np.empty((0,10,20,2))
+video_file = '011.mp4'
 
-np.append(b, np.array([a]), axis=0)
+cap = cv2.VideoCapture(video_file)
+ret, frame1 = cap.read()
+frame1 = cv2.resize(frame1, (0, 0), fx=0.25, fy=0.25)
+prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+hsv = np.zeros_like(frame1)
+hsv[...,1] = 255
+
+while True:
+    ret, frame2 = cap.read()
+    if not ret:
+        break
+    frame2 = cv2.resize(frame2, (0, 0), fx=0.25, fy=0.25)
+    next = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+
+    flow = cv2.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+    mag = np.sqrt(flow[...,0]**2 + flow[...,1]**2)
+    hsv[...,0] = ang*180/np.pi/2
+    hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+    rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+    cv2.imshow('flow',rgb)
+    cv2.imshow('vid', next)
+
+    if cv2.waitKey(2) & 0xFF == ord('q'):
+        break
