7235b95613c36c84aa98dad55dbe9976ad62c024
lingfliu <lingfeng.liu@163.com>
2023-08-21 08:47:35 +0800

    fixed batch counting err

--- model_train.py
+++ model_train.py
@@ -20,7 +20,6 @@ y = torch.from_numpy(to_categorical([l-1 for l in labels], 5)).to(net.device)
 
 epoch_size = 500
 batch_size = 400
-batches = len(flow_index)//batch_size
 
 # manually split train & test videos
 # take the first 25 videos as the training dataset, the last 6 videos as the test dataset
@@ -35,6 +34,7 @@ train_idx = [i for i in range(idx)]
 test_idx = [i+idx for i in range(label_collapsed.shape[0]-idx)]
 
 
+batches = len(train_idx)//batch_size
 for ep in range(epoch_size):
 
     net.train()
@@ -67,16 +67,17 @@ for ep in range(epoch_size):
 
     net.eval()
 
-    val_batches = len(test_idx) //batch_size
+    val_batch_size = 200
+    val_batches = len(test_idx) //val_batch_size
     acc_vals = []
     loss_vals = []
     for b in range(val_batches):
-        batch_x= flows[test_idx[b*batch_size:b*batch_size+batch_size]].astype(np.float32)
+        batch_x= flows[test_idx[b*val_batch_size:b*val_batch_size+val_batch_size]].astype(np.float32)
         batch_x = batch_x.transpose(0, 3, 1, 2)
         batch_x = torch.from_numpy(batch_x).float()
         batch_x = batch_x.to(net.device)
 
-        batch_y = y[test_idx[b*batch_size:b*batch_size+batch_size]]
+        batch_y = y[test_idx[b*val_batch_size:b*val_batch_size+val_batch_size]]
 
         o = net(batch_x)
         pred = torch.argmax(o, 1)
@@ -91,15 +92,4 @@ for ep in range(epoch_size):
     acc_avg_val = np.average(np.array(acc_vals))
     loss_avg_val = np.average(np.array(loss_vals))
     print('ep {} val loss = {}, acc = {}'.format(ep, loss_avg_val, acc_avg_val))
-
-
-
-
-
-
-
-
-
-
-
 
